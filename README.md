314e45b35e2e9ecd16cbb0e099285e2d9eb452b8
Liat Sayfan <lsayfan@gmail.com>
2024-10-07 16:17:37 -0700

    initial version

---
+++ README.md
@@ -0,0 +1,62 @@
+* Categorize oraganizations into: synagogues, schools, human service agencies, JCCs, youth 
+  serving organizations, single group advocacy organizations,cultural organizations.
+* For any given organization, several types of analysis are possible, for example:
+
+ • The number of unique individuals/households on their list
+ • The proportion of individuals/households on their lists that are only on their lists and the 
+ proportion that appear on other lists
+ • The distribution of the number of other organizational lists (1, 2, 3, 4, etc.) that their 
+ individuals/households also appear on
+ • The distribution of types of organizations that their individuals/households also appear on
+ • The geographic distribution of individuals/households on their lists (assuming geographic 
+ data exist)
+ • Averages or medians for all organizations that can be used as benchmarks for comparison (e.g.,
+ a given organization could compare the proportion of its lists that is unique to it against the
+ median unique proportion across all lists)
+ 
+Several analyses examining the contacts list altogether can also be prepared for CJP, for example:
+ • The total number of unique households across all lists
+ • The distribution of the number of lists (1, 2, 3, 4, etc.) that individuals/households appear on
+ • The distribution of the number of types of organizations (1, 2, 3, 4, etc.) that 
+ individuals/households appear on
+ • The proportion of all individuals/households that appear on the different types of 
+ organizations (e.g., what proportion appear on synagogue lists or social service lists)
+ • Among individuals/households that appear on more than one list, substantial clusters of types 
+ of organizations they appear on
+ • The geographic distribution of individuals/households across all lists combined and within 
+ each type of organization (assuming geographic data exist)
+# Plan
+* [x] set up python virtual environment
+* [x] convert xslx files to csv
+- [x] generate a list of organization by enumerating all the input file
+- [ ] Merge all files where each row contains all the personal details for each person and 
+ which organization lists that person appeared on (marked as 0=not on that org list or 1=yes on 
+  that org list). 
+- [ ] Scan all the files. For each file iterate over all the people and store info in a shared dict 
+  where the key is the person's details as a tuple and the value is a list of organizations (the 
+  file names the person appears on)  
+{(first_name, last_name, address, email, phone): [org1,org2,]}
+- [ ] Iterate over the entries of the dictionary. 
+  - [ ] Each entry will become single row in the output file. the dictionary key will become the 
+    same fields in the row. 
+  - [ ] Iterate over the organization in the value and set the corresponding fields to "1" in 
+    the row
+  - [ ] We need a dict that maps org name to field indices in the output file (generate from the 
+    org list)
+  - [ ] Add another field that will be the number of orgs (len of the dict value)
+- the end result will be a csv file 
+
+# creating virtual environment
+ ```
+ python -m venv venv
+ ```
+# Activate the virtual environment
+```
+ .\venv\scripts\activate.ps1
+ python
+```
+# Install dependencies
+```
+pip install -r requirments.txt
+```
+
---
+++ main.py
@@ -0,0 +1,154 @@
+from datetime import datetime
+import json
+import os
+import time
+from collections import defaultdict
+from glob import glob
+from pprint import pprint as pp
+import pandas as pd
+from config import (
+    col_list,
+    input_dir,
+    target_dir,
+    org_names_file,
+    valid_orgs_file,
+    all_people_json,
+    all_people_csv,
+    duplicates_json,
+    key_separator
+)
+
+
+def convert_files():
+    os.makedirs(target_dir, exist_ok=True)
+    # Read file list from target directory (*.xlsx)
+    files = glob(f'{input_dir}/*.xlsx')
+    for f in files[1:]:
+        sheets = pd.read_excel(f, sheet_name=None)  # Returns a dictionary of DataFrames
+        df = list(sheets.values())[0]  # Save each sheet as a CSV
+        base_name = os.path.basename(f)
+        csv_file = f'{input_dir}/output/{base_name.replace(".xlsx", ".csv")}'
+        if os.path.isfile(csv_file):
+            continue
+        print('converting:', csv_file)
+        df.to_csv(csv_file, index=False)
+
+
+def generate_org_list():
+    files = glob(f'{input_dir}/*.xlsx')
+    org_list = []
+    for f in files:
+        org_name = os.path.basename(f).split('.')[0]
+        org_list.append(org_name)
+    s = "\n".join(org_list)
+    open(org_names_file, 'w').write(s)
+
+
+def merge_files():
+    """Merge all files where each row contains all the personal details for each person and
+      which organization lists that person appeared on (marked as 0=not on that org list or 1=yes on
+      that org list.
+    """
+    min_col_set = {'First Name', 'Last Name'}
+    orgs = open(org_names_file).read().split('\n')
+
+    all_people = defaultdict(list)
+    valid_orgs = []
+    for org in orgs:
+        df = pd.read_csv(f'{input_dir}/output/{org}.csv').fillna('').astype(str)
+        if not min_col_set.issubset(set(df.columns)):
+            continue
+
+        print('Processing org:', org)
+        valid_orgs.append(org)
+        for index, row in df.iterrows():
+            key = key_separator.join(row.get(col, '') for col in col_list)
+            all_people[key].append(org)
+
+    open(valid_orgs_file, 'w').write("\n".join(valid_orgs))
+    json.dump(all_people, open(all_people_json, 'w'))
+
+
+def generate_output_file():
+    """ Create a DataFrame from the dictionary where the columns the details of a person + all org
+        names and the values are 0 or 1 depending on whether the person is on that org list
+    """
+    all_people = json.load(open(all_people_json))
+    valid_orgs = open(org_names_file).read().split('\n')
+    cols = col_list + valid_orgs
+    df = pd.DataFrame(columns=cols)
+    total_people = len(all_people)
+    for i, (k, v) in enumerate(all_people.items(), start=1):
+        if i % 10000 == 0:
+            curr_time = datetime.now().strftime('%H:%M')
+            print(f"[{curr_time}] {i:,} / {total_people:,}, {i * 100 / total_people:.2f}% complete")
+        row = k.split(key_separator) + [1 if org in v else 0 for org in valid_orgs]
+        try:
+            df.loc[len(df)] = row
+        except Exception as e:
+            print(row)
+            raise
+
+    df.to_csv(all_people_csv, index=False)
+
+
+def list_org_columns():
+    """List orgs that don't have at least 'First Name' and 'Last Name' columns"""
+    orgs = open(org_names_file).read().split('\n')
+    min_cols = {'First Name', 'Last Name'}
+    for org in orgs:
+        df = pd.read_csv(f'{input_dir}/output/{org}.csv')
+        cols = set(list(df.columns))
+        if cols.issuperset(min_cols):
+            continue
+        print(org, ':', list(df.columns))
+        print('-' * 10)
+
+
+def find_suspected_duplicates():
+    all_people = [p.split(key_separator) for p in json.load(open(all_people_json)).keys()]
+
+    # Process fields to remove leading/trailing spaces and convert to lowercase
+    all_people = [[field.strip().lower() for field in person] for person in all_people]
+
+    # Create 3 dictionaries where the keys are first name + last name, email address, phone number
+    all_by_name = defaultdict(list)
+    all_by_email = defaultdict(list)
+    all_by_phone = defaultdict(list)
+
+    for p in all_people:
+        all_by_name[f'{p[0]} {p[1]}'].append(p)
+        all_by_email[p[3]].append(p)
+        all_by_phone[p[4]].append(p)
+
+    invalid_phone_values = ['', 'none', 'y', 'no call', 'null, null', 'x', 'nocall']
+    invalid_email_values = ['', 'none', 'nan', 'n/a']
+
+    all_by_name = {k: v for k, v in all_by_name.items() if len(v) > 1}
+    all_by_email = {k: v for k, v in all_by_email.items() if
+                    k not in invalid_email_values and len(v) > 1}
+    all_by_phone = {k: v for k, v in all_by_phone.items() if
+                    k not in invalid_phone_values and len(v) > 1}
+
+    all_duplicates = dict(by_name=all_by_name, by_email=all_by_email, by_phone=all_by_phone)
+
+    print('Duplicates by name:', f'{len(all_by_name):,}')
+    print('Duplicates by email:', f'{len(all_by_email):,}')
+    print('Duplicates by phone:', f'{len(all_by_phone):,}')
+
+    # Save the duplicates to a JSON file
+    json.dump(all_duplicates, open(duplicates_json, 'w'), indent=2)
+
+
+def main():
+    """ """
+    # convert_files()
+    # generate_org_list()
+    # list_org_columns()
+    # merge_files()
+    # generate_output_file()
+    find_suspected_duplicates()
+
+
+if __name__ == "__main__":
+    main()
